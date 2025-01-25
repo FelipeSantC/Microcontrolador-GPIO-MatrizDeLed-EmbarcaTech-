fac55b846a8c53d7ead358a308eb9cf62a4081c6
Alisson Livio <livilia74629@gmail.com>
2025-01-25 18:19:11 -0300

    feat: criação da função main e inicialização da porta PIO

--- Animation1.c
+++ Animation1.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "hardware/clocks.h"
+#include "hardware/adc.h"
+#include "pico/bootrom.h"
 
+#include "pio_matrix.pio.h"
+
+//Matriz de LED 
+#define NUM_PIXELS 25
+#define OUT_PIN 9
+
+
+void initPio(){
+
+    PIO pio = pio0; 
+
+    //configurações da PIO
+    uint offset = pio_add_program(pio, &pio_matrix_program);
+    uint sm = pio_claim_unused_sm(pio, true);
+    pio_matrix_program_init(pio, sm, offset, OUT_PIN);
+
+}
 int main(){
+
+    bool ok;
+    uint16_t i;
+    uint32_t valor_led;
+    double r = 0.0, b = 0.0 , g = 0.0;
+
+    stdio_init_all();
 
     return 0;
 }
