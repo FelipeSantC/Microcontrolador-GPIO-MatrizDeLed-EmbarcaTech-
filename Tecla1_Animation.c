58fa2383f6ea53426c48095a4e626b3b286a4a28
Alisson Livio <livilia74629@gmail.com>
2025-01-26 19:36:37 -0300

    feat: adição dos frames

---
+++ Tecla1_Animation.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "hardware/adc.h"
+#include "pico/bootrom.h"
+
+//arquivo .pio
+#include "pio_matrix.pio.h"
+
+#define NUM_PIXELS 25
+
+// Declaração da função desenhar_matriz
+void desenhar_matriz(PIO pio, uint sm, const float *desenho, float r, float g, float b);
+
+// Função de animação com 5 frames (efeito de onda)
+void animacao1(PIO pio, uint sm) {
+    // Definindo os 5 frames da animação
+    // Vetores de imagens para matriz de LEDs
+    const float frame1[NUM_PIXELS] = {
+        0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0, 0.0,
+        0.0, 1.0, 1.0, 1.0, 0.0,
+        1.0, 1.0, 1.0, 1.0, 1.0,
+    };
+
+    const float frame2[NUM_PIXELS] = {
+       1.0, 1.0, 1.0, 1.0, 1.0,
+       0.0, 1.0, 1.0, 1.0, 0.0,
+       0.0, 0.0, 1.0, 0.0, 0.0, 
+       0.0, 0.0, 0.0, 0.0, 0.0,
+       0.0, 0.0, 0.0, 0.0, 0.0,
+    };
+    const float frame3[NUM_PIXELS] = {
+        1.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 1.0, 1.0,
+        1.0, 1.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 1.0, 1.0,
+        1.0, 0.0, 0.0, 0.0, 0.0,
+    };
+
+    const float frame4[NUM_PIXELS] = {
+        0.0, 0.0, 0.0, 0.0, 1.0,
+        1.0, 1.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 1.0, 1.0, 1.0,
+        1.0, 1.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 1.0,
+    };
+    const float frame5[NUM_PIXELS] = {
+        1.0, 0.0, 0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 1.0, 0.0, 0.0,
+        0.0, 1.0, 0.0, 1.0, 0.0,
+        1.0, 0.0, 0.0, 0.0, 1.0,
+    };
+
+    // Array de frames
+    const float *frames[5] = {frame1, frame2, frame3, frame4, frame5};
+
+    // Exibindo a animação
+    for (int i = 0; i < 5; i++) {
+        desenhar_matriz(pio, sm, frames[i], 0.0, 1.0, 0.0); // Verde
+        sleep_ms(200); // Intervalo entre os frames
+    }
+}
