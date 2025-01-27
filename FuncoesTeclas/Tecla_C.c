de27a4e2bb998574a17b87786a3419ed7923c09a
Alisson Livio <livilia74629@gmail.com>
2025-01-26 21:08:23 -0300

    adicionando novos arquivos

---
+++ FuncoesTeclas/Tecla_C.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+
+#define NUM_PIXELS 25
+
+// Declaração da função desenhar_matriz
+void desenhar_matriz(PIO pio, uint sm, const uint32_t *desenho);
+
+// Função de animação com 5 frames (efeito de onda)
+void animacao_C(PIO pio, uint sm) {
+    // Definindo os 5 frames da animação
+    // Vetores de imagens para matriz de LEDs
+    const uint32_t frame1[NUM_PIXELS] = {
+        0x00CC0000, 0x00CC0000, 0x00CC0000, 0x00CC0000, 0x00CC0000,
+        0x00CC0000, 0x00CC0000, 0x00CC0000, 0x00CC0000, 0x00CC0000, 
+        0x00CC0000, 0x00CC0000, 0x00CC0000, 0x00CC0000, 0x00CC0000,
+        0x00CC0000, 0x00CC0000, 0x00CC0000, 0x00CC0000, 0x00CC0000,
+        0x00CC0000, 0x00CC0000, 0x00CC0000, 0x00CC0000, 0x00CC0000
+    };
+    // Exibindo a animação
+    desenhar_matriz(pio, sm, frame1); 
+    sleep_ms(200); // Intervalo entre os frames
+}
